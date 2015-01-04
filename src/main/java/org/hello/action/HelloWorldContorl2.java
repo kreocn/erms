8212657ce8883f5635229758a39436a7746f4d12
kreocn <kreo@qq.com>
2015-01-04 15:36:53 +0800

    fix db conifg

--- src/main/java/org/hello/action/HelloWorldContorl2.java
+++ src/main/java/org/hello/action/HelloWorldContorl2.java
@@ -9,6 +9,7 @@ public class HelloWorldContorl2 {
 	@RequestMapping("/helloWorld2")
 	public String helloWorld(Model model) {
 		// org.springframework.jd
+		// org/aspectj/weaver/reflect/ReflectionWorld$ReflectionWorldException
 		model.addAttribute("name", "Hello World!");
 		return "helloWorld2";
 	}
